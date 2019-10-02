a2d09755db8fc2512cbd24cea1ee61d2f05f3570
Adnan Muhib <adnan.muhib@rocketmail.com>
2019-10-02 17:31:59 +0500

    ADD: Model for the Leads Table

---
+++ models/Lead.js
@@ -0,0 +1,16 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  var Lead = sequelize.define("Lead", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return Lead;
+};
