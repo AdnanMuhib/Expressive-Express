53917678a02584b69e3f8bde4d8965a217dc6a9a
Adnan Muhib <adnan.muhib@rocketmail.com>
2019-10-02 17:48:33 +0500

    Update: implement /leads route, Show a collection of leads upon submission

--- controllers/landing.js
+++ controllers/landing.js
@@ -8,6 +8,12 @@ exports.submit_lead = function(req, res, nex) {
   return models.Lead.create({
     email: req.body.lead_email
   }).then(lead => {
-    res.redirect("/");
+    res.redirect("/leads");
+  });
+};
+
+exports.show_leads = function(req, res, next) {
+  models.Lead.findAll().then(leads => {
+    res.render("landing", { title: "Express", leads: leads });
   });
 };
