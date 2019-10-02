dbc09c41f2442016230a3460c3663badf693a144
Adnan Muhib <adnan.muhib@rocketmail.com>
2019-10-02 17:41:06 +0500

    Update: Save the collected leads to the database

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,8 +1,13 @@
+const models = require("../models");
+
 exports.get_landing = function(req, res, next) {
   res.render("landing", { title: "Express" });
 };
 
 exports.submit_lead = function(req, res, nex) {
-  console.log("lead email:", req.body.lead_email);
-  res.redirect("/");
+  return models.Lead.create({
+    email: req.body.lead_email
+  }).then(lead => {
+    res.redirect("/");
+  });
 };
