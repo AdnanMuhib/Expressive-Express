8eedfc398994ccb5bbc97228be2503d74dddd977
Adnan Muhib <adnan.muhib@rocketmail.com>
2019-10-02 16:03:44 +0500

    Update: add post email lead route to landing page

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,8 @@
 exports.get_landing = function(req, res, next) {
   res.render("landing", { title: "Express" });
 };
+
+exports.submit_lead = function(req, res, nex) {
+  console.log("lead email:", req.body.lead_email);
+  res.redirect("/");
+};
