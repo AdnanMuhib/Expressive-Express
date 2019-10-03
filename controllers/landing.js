0f6b849fc3d566a03d5db7a4c22e7ae8a7464ea0
Adnan Muhib <adnan.muhib@rocketmail.com>
2019-10-03 17:39:55 +0500

    Update: Migrate all pages to Bootstrap

--- controllers/landing.js
+++ controllers/landing.js
@@ -14,7 +14,7 @@ exports.submit_lead = function(req, res, next) {
 
 exports.show_leads = function(req, res, next) {
   models.Lead.findAll().then(leads => {
-    res.render("landing", { title: "Express", leads: leads });
+    res.render("lead/leads", { title: "Express", leads: leads });
   });
 };
 
@@ -24,7 +24,7 @@ exports.show_lead = function(req, res, next) {
       id: req.params.lead_id
     }
   }).then(lead => {
-    res.render("lead", { lead: lead });
+    res.render("lead/lead", { lead: lead });
   });
 };
 
@@ -34,7 +34,7 @@ exports.show_edit_lead = function(req, res, next) {
       id: req.params.lead_id
     }
   }).then(lead => {
-    res.render("edit_lead", { lead: lead });
+    res.render("lead/edit_lead", { lead: lead });
   });
 };
 
