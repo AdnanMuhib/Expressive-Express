a8dbd0388b21be00e19776bcff48a379a89964f0
Adnan Muhib <adnan.muhib@rocketmail.com>
2019-10-03 19:49:06 +0500

    Add Login and Signup pages and routes

---
+++ controllers/user.js
@@ -0,0 +1,9 @@
+const models = require("../models");
+
+exports.show_login = function(req, res, next) {
+  res.render("user/login", { formData: {}, errors: {} });
+};
+
+exports.show_signup = function(req, res, next) {
+  res.render("user/signup", { formData: {}, errors: {} });
+};
