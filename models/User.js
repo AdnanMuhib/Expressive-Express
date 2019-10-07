e3756349a03f73f9b0f936e3ebb454373f611e4f
Adnan Muhib <adnan.muhib@rocketmail.com>
2019-10-07 17:54:19 +0500

    ADD: User Model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define("User", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true
+    }
+  });
+  return user;
+};
