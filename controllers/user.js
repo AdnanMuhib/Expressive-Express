8d5ed58510e10f6b14d6d58cd8debc05e655e267
Adnan <adnan.muhib@rocketmail.com>
2019-11-08 01:36:39 +0500

    Implement error handling and form validation for sign up route

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,8 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 const myPassport = require("../passport_setup")(passport);
 let flash = require("connect-flash");
+const { validateUser } = require("../validators/signup");
+let { isEmpty } = require("lodash");
 
 exports.show_login = function(req, res, next) {
   res.render("user/login", { formData: {}, errors: {} });
@@ -11,9 +13,15 @@ exports.show_login = function(req, res, next) {
 exports.show_signup = function(req, res, next) {
   res.render("user/signup", { formData: {}, errors: {} });
 };
+
+const rerender_signup = function(errors, req, res, next) {
+  res.render("user/signup", { formData: req.body, errors: errors });
+};
+
 const generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
+
 exports.login = function(req, res, next) {
   passport.authenticate("local", {
     successRedirect: "/",
@@ -21,17 +29,25 @@ exports.login = function(req, res, next) {
     failureFlash: true
   })(req, res, next);
 };
+
 exports.signup = function(req, res, next) {
-  const newUser = models.User.build({
-    email: req.body.email,
-    password: generateHash(req.body.password)
-  });
-  return newUser.save().then(result => {
-    passport.authenticate("local", {
-      successRedirect: "/",
-      failureRedirect: "/signup",
-      failureFlash: true
-    })(req, res, next);
+  let errors = {};
+  return validateUser(errors, req).then(errors => {
+    if (!isEmpty(errors)) {
+      rerender_signup(errors, req, res, next);
+    } else {
+      const newUser = models.User.build({
+        email: req.body.email,
+        password: generateHash(req.body.password)
+      });
+      return newUser.save().then(result => {
+        passport.authenticate("local", {
+          successRedirect: "/",
+          failureRedirect: "/signup",
+          failureFlash: true
+        })(req, res, next);
+      });
+    }
   });
 };
 exports.logout = function(req, res, next) {
